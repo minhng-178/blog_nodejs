3b01693a1ce6888e8f1defd85100076b1f3175a5
minhng-178 <anhminh1782002@gmail.com>
2023-08-09 11:20:06 +0700

    11:20AM

---
+++ src/config/db/index.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+async function connect() {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://blog_dev:blog@cluster0.vmko02b.mongodb.net/"
+    );
+    console.log("Connect successfully!!!");
+  } catch (error) {
+    console.log("Connect failure!!!");
+  }
+}
+
+module.exports = { connect };
--- src/index.js
+++ src/index.js
@@ -2,25 +2,42 @@ const path = require("path");
 const express = require("express");
 const { engine } = require("express-handlebars");
 const morgan = require("morgan");
+const methodOverride = require("method-override");
 
 const app = express();
 const port = 3000;
+
+const route = require("./routes");
+
+const db = require("./config/db");
+db.connect();
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
+app.use(methodOverride("_method"));
 
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(morgan("combined"));
 
-app.engine(".hbs", engine({ extname: ".hbs" }));
+app.engine(
+  ".hbs",
+  engine({
+    extname: ".hbs",
+    helpers: {
+      sum: (a, b) => a + b,
+    },
+  })
+);
 app.set("view engine", ".hbs");
-app.set("views", path.join(__dirname, "resources/views"));
+app.set("views", path.join(__dirname, "resources", "views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
-app.get("/news", (req, res) => {
-  res.render("news");
-});
+route(app);
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
